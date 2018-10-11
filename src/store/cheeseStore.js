ef31f94435b8ff4d7a914ebd93e2a9832c692e4c
Gabriel Morris <gabemorris@outlook.com>
2018-10-11 11:48:05 -0700

    #1 remove the cheeeeese

--- src/store/cheeseStore.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { cheeseReducer } from '../reducers/cheese';
-
-export default createStore(cheeseReducer, applyMiddleware(thunk));
