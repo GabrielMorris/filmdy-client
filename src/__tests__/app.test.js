cb6c6d4a87421e9020a47aafdc5449cfa14b921a
Gabriel Morris <gabemorris@outlook.com>
2018-10-23 10:38:32 -0700

    oopsies in my config file

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import App from '../components/app';
+
+// describe('<NoMatch />', () => {
+//   it('Renders without crashing', () => {
+//     shallow(<NoMatch />);
+//   });
+
+//   it('Renders a not-found-container', () => {
+//     const wrapper = shallow(<NoMatch />);
+//     expect(wrapper.find('.not-found-container').length).toEqual(1);
+//   });
+
+//   it('Renders an error message', () => {
+//     const wrapper = shallow(<NoMatch />);
+//     expect(wrapper.find('h1').text()).toEqual('Error');
+//   });
+
+//   it('Render 404 not found and an error message', () => {
+//     const wrapper = shallow(<NoMatch />);
+//     expect(wrapper.find('.not-found-error').length).toEqual(2);
+//   });
+// });
